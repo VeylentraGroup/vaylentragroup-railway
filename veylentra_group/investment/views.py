0b4572f871d8ff787d97bd6e73078f5858d22bd1
VeylentraGroup <cryptochica72@gmail.com>
2026-08-13 22:23:07 +0100

    Fix duplicate wallet on deposit success

--- veylentra_group/investment/views.py
+++ veylentra_group/investment/views.py
@@ -1061,9 +1061,10 @@ def approve_transaction_view(request, transaction_id):
 
 
 
+from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from investment.models import Wallet
 
-
-from django.shortcuts import get_object_or_404
 
 @login_required
 def deposit_success(request):
@@ -1072,18 +1073,47 @@ def deposit_success(request):
     user_name = request.GET.get('user_name')
     plan_name = request.GET.get('plan_name')
 
-    # Get the wallet object for the selected wallet address
-    from investment.models import Wallet  # adjust import
-    wallet = get_object_or_404(Wallet, wallet_address=wallet_address)
+    # Find the wallet without requiring wallet_address to be unique.
+    wallet = (
+        Wallet.objects
+        .filter(wallet_address=wallet_address)
+        .order_by('id')
+        .first()
+    )
 
     return render(request, 'investment/deposit_success.html', {
         'deposit_amount': deposit_amount,
-        'wallet_address': wallet.wallet_address,
-        'wallet_name': wallet.name,
+        'wallet_address': wallet_address,
+        'wallet_name': wallet.name if wallet else '',
         'user_name': user_name,
         'plan_name': plan_name,
-        'wallet': wallet,  # pass the object to the template
+        'wallet': wallet,
     })
+
+
+
+
+# from django.shortcuts import get_object_or_404
+
+# @login_required
+# def deposit_success(request):
+#     deposit_amount = request.GET.get('deposit_amount')
+#     wallet_address = request.GET.get('wallet_address')
+#     user_name = request.GET.get('user_name')
+#     plan_name = request.GET.get('plan_name')
+
+#     # Get the wallet object for the selected wallet address
+#     from investment.models import Wallet  # adjust import
+#     wallet = get_object_or_404(Wallet, wallet_address=wallet_address)
+
+#     return render(request, 'investment/deposit_success.html', {
+#         'deposit_amount': deposit_amount,
+#         'wallet_address': wallet.wallet_address,
+#         'wallet_name': wallet.name,
+#         'user_name': user_name,
+#         'plan_name': plan_name,
+#         'wallet': wallet,  # pass the object to the template
+#     })
 
 
 
